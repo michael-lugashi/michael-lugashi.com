0d3c62bd2a16680b2adea231352b9aacd20da141
michael-lugashi <michael.lugashi@gmail.com>
2025-09-01 16:21:45 +0300

    changed max width

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="min-h-screen bg-white-p">
-      <div className="pt-16 px-16 max-w-[1200px] mx-auto">
+      <div className="px-16 max-w-[1500px] mx-auto">
         <Header />
       </div>
     </div>
