883842c4d581658e87d060245920f36706e2b525
michael-lugashi <michael.lugashi@gmail.com>
2025-09-01 14:55:11 +0300

    created scroll component

---
+++ src/components/ScrollIndicator.tsx
@@ -0,0 +1,10 @@
+const ScrollIndicator: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-min ml-36">
+      <span className="text-gray-p tracking-[0.5em] mb-4 text-sm">SCROLL</span>
+      <div className="w-px h-96 bg-gray-p mr-1" />
+    </div>
+  );
+};
+
+export default ScrollIndicator;
