a905b4a2075f32a7f5ee8e2137441ef98fdd0802
michael-lugashi <michael.lugashi@gmail.com>
2025-09-03 15:34:10 +0300

    changed card color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ children, className = '' }) => {
   return (
     <div
-      className={`backdrop-blur-sm bg-brown-s/12 rounded-3xl p-8 md:p-12 shadow-2xl border border-white-p/20 ${className}`}
+      className={`backdrop-blur-sm  bg-gradient-to-br from-transparent to-brown-s/30 rounded-3xl p-8 md:p-12 shadow-2xl${className}`}
     >
       {children}
     </div>
