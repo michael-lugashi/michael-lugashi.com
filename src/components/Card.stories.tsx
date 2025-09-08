d2abd4b54a124d39683a47921dc5fcb7995a2ef3
michael-lugashi <michael.lugashi@gmail.com>
2025-09-08 14:36:37 +0300

    added storybook for card component

---
+++ src/components/Card.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import Card from './Card';
+
+const meta: Meta<typeof Card> = {
+  title: 'Components/Card',
+  component: Card,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <div className="p-6 bg-white-p">
+      <Card>
+        <div className="text-brown-p">This is inside a Card component.</div>
+      </Card>
+    </div>
+  ),
+};
