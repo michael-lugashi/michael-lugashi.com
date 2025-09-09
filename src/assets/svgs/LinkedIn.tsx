0e4a49442f9bc34bb72698b6c6f4d804a161a6b9
michael-lugashi <michael.lugashi@gmail.com>
2025-09-09 11:45:33 +0300

    added svgs

---
+++ src/assets/svgs/LinkedIn.tsx
@@ -0,0 +1,7 @@
+const LinkedIn = ({ className = '' }) => (
+  <svg className={className} width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2ZM8.34 18.34H5.67V9.67h2.67v8.67ZM7 8.45A1.56 1.56 0 1 1 7 5.33a1.56 1.56 0 0 1 0 3.12Zm11.34 9.89h-2.67v-4.5c0-1.07-.02-2.45-1.49-2.45-1.49 0-1.72 1.16-1.72 2.37v4.58H9.8V9.67h2.56v1.18h.04c.36-.68 1.25-1.4 2.58-1.4 2.76 0 3.27 1.82 3.27 4.18v4.72Z" />
+  </svg>
+);
+
+export default LinkedIn;
