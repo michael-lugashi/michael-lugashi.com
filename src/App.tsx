df3b93191298abb42492182d270c4fe73a17eada
michael-lugashi <michael.lugashi@gmail.com>
2025-09-14 16:40:09 +0300

    changed z index of theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     <div className="min-h-screen bg-white-p dark:bg-black-p">
       <NavBar handleScrollToContactMe={handleScrollToContactMe} />
       <div className="relative px-4 tablet:px-8 laptop:px-16 max-w-[1500px] flex flex-col gap-40 mx-auto pb-20">
-        <ThemeToggleBar className="absolute top-17 right-6 laptop:right-1/10 short:top-[7vh] tall:top-[20vh] z-50" />
+        <ThemeToggleBar className="absolute top-17 right-6 laptop:right-1/10 short:top-[7vh] tall:top-[20vh] z-20" />
         <Header />
         <Projects projects={projects} />
         <AboutMe />
