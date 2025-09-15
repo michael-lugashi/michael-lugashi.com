16193c12b092289da92be73231558467f210b004
michael-lugashi <michael.lugashi@gmail.com>
2025-09-15 14:50:53 +0300

    fixed striped box

--- src/components/StripedBox.tsx
+++ src/components/StripedBox.tsx
@@ -1,7 +1,7 @@
 export default function StripedBox({ className }: { className: string }) {
   return (
     <div
-      className={`bg-[repeating-linear-gradient(45deg,theme(colors.pink-p-transparent)_0px,theme(colors.pink-p-transparent)_3px,transparent_3px,transparent_10px)] dark:bg-[repeating-linear-gradient(45deg,theme(colors.brown-p-transparent)_0px,theme(colors.brown-p-transparent)_3px,transparent_3px,transparent_10px)] ${className}`}
+      className={`bg-[repeating-linear-gradient(45deg,var(--color-pink-p)_0px,var(--color-pink-p)_3px,transparent_3px,transparent_9px)] dark:bg-[repeating-linear-gradient(45deg,theme(colors.brown-p-transparent)_0px,theme(colors.brown-p-transparent)_3px,transparent_3px,transparent_9px)] ${className}`}
     />
   );
 }
