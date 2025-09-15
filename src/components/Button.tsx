e42dc04c381234856a4b907a250dae8574bb9b29
michael-lugashi <michael.lugashi@gmail.com>
2025-09-15 15:01:01 +0300

    fixed button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,7 +22,7 @@ const Button: React.FC<ButtonProps> = ({
     className={`transition-transform cursor-pointer duration-300 transform px-12 py-3 text-xl font-bold rounded-lg text-brown-p dark:text-pink-p border-2 border-brown-p dark:border-pink-p ${className} ${
       disabled
         ? 'hover:scale-100 active:scale-100 cursor-not-allowed bg-gray-400/40'
-        : 'tablet:hover:scale-110 tablet:active:scale-95 active:scale-80 bg-[repeating-linear-gradient(45deg,var(--color-pink-p)_0px,var(--color-pink-p)_3px,transparent_3px,transparent_9px)] hover:bg-none active:bg-none hover:bg-pink-p/50 active:bg-pink-p/50 dark:bg-[repeating-linear-gradient(45deg,var(--color-brown-p)_0px,var(--color-brown-p-transparent)_3px,transparent_3px,transparent_9px)] dark:hover:bg-brown-p/40 dark:active:bg-brown-p/40'
+        : 'tablet:hover:scale-110 tablet:active:scale-95 active:scale-80 bg-[repeating-linear-gradient(45deg,var(--color-pink-p)_0px,var(--color-pink-p)_3px,transparent_3px,transparent_9px)] hover:bg-none active:bg-none hover:bg-pink-p/50 active:bg-pink-p/50 dark:bg-[repeating-linear-gradient(45deg,var(--color-brown-p-transparent)_0px,var(--color-brown-p-transparent)_3px,transparent_3px,transparent_9px)] dark:hover:bg-brown-p/40 dark:active:bg-brown-p/40'
     }`}
   >
     <span className="relative z-10">
