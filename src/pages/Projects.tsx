abe2aab6376f0319dcbd54152918e0f71b0bf1e4
michael-lugashi <michael.lugashi@gmail.com>
2025-09-15 15:06:38 +0300

    added text for projects section

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -16,14 +16,10 @@ const Projects: React.FC<{ projects: Project[] }> = ({ projects }) => {
     <section className="-mt-30">
       <div className="mb-20 flex flex-row justify-between items-stretch">
         <Title
-          title="Projects"
+          title="Portfolio"
           align="left"
           className="laptop:w-1/2"
-          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum"
+          subtitle="I build scalable, reliable applications that balance performance with great user experience — from intuitive user interfaces to robust servers, databases, and cloud infrastructure."
         />
         <StripedBox className="hidden laptop:block w-1/3 my-8" />
       </div>
