5c6d02142f4efc5332576dad8ecc5ba79ddff47d
michael-lugashi <michael.lugashi@gmail.com>
2025-09-30 15:17:00 +0300

    added creds for email service

---
+++ src/config/email.ts
@@ -0,0 +1,7 @@
+const emailConfig = {
+  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID as string,
+  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string,
+  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string,
+};
+
+export default emailConfig;
